3c6985d41cde1bd76069060a53827d3638ed9eeb
Vladimir.Uznichenko <darknerty@gmail.com>
2018-04-17 23:30:32 +0300

    Task-2. Continue

--- src/blocks/rangeSlider/js/index.js
+++ src/blocks/rangeSlider/js/index.js
@@ -5,17 +5,46 @@ $(".rangeSlider__input").ionRangeSlider({
     grid: true,
     grid_num:2,
     grid_margin:false,
-    from: 290000000,
-    disable: true
+    from: 30000000,
+    disable:true
 });
 
 $(document).ready(function(){
     var w = parseFloat($('.irs-slider').get(0).style.left);
-    var max=100;
+    var max=95;
     var middle=50;
-    if(w>middle){
-        $('.irs-grid-pol:first-child').css('background-color','#23dbb9');
-        $('.irs-grid-pol:nth-child(2)').css('background-color','#23dbb9');
-    }
+      if(w<middle){
+            $('.irs-grid-pol:first-child').css('background-color','#23dbb9');
+            $('.irs-grid-pol:nth-child(7)').css('background-color','#2b354a');
+            $('.irs-grid-pol:nth-child(13)').css('background-color','#2b354a'); 
+        }
+        if(w>=middle){
+            $('.irs-grid-pol:first-child').css('background-color','#23dbb9');
+            $('.irs-grid-pol:nth-child(7)').css('background-color','#23dbb9');
+            $('.irs-grid-pol:nth-child(13)').css('background-color','#2b354a'); 
+        }
+        if(w>=max){
+            $('.irs-grid-pol:first-child').css('background-color','#23dbb9');
+            $('.irs-grid-pol:nth-child(7)').css('background-color','#23dbb9'); 
+            $('.irs-grid-pol:nth-child(13)').css('background-color','#23dbb9');
+        }
+        $('.irs-slider').click(function(){
+            var e = parseFloat($('.irs-slider').get(0).style.left);
+            if(e<middle){
+                $('.irs-grid-pol:first-child').css('background-color','#23dbb9');
+                $('.irs-grid-pol:nth-child(7)').css('background-color','#2b354a');
+                $('.irs-grid-pol:nth-child(13)').css('background-color','#2b354a'); 
+            }
+            if(e>=middle){
+                $('.irs-grid-pol:first-child').css('background-color','#23dbb9');
+                $('.irs-grid-pol:nth-child(7)').css('background-color','#23dbb9');
+                $('.irs-grid-pol:nth-child(13)').css('background-color','#2b354a'); 
+            }
+            if(e>=max){
+                $('.irs-grid-pol:first-child').css('background-color','#23dbb9');
+                $('.irs-grid-pol:nth-child(7)').css('background-color','#23dbb9'); 
+                $('.irs-grid-pol:nth-child(13)').css('background-color','#23dbb9');
+            }
+        });
 
 });
