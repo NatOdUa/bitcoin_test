dbad53be64c3fe0be8911cdda51bca2ce168594a
Vladimir.Uznichenko <darknerty@gmail.com>
2018-04-18 23:44:58 +0300

    Task-3. Continue

--- src/blocks/dropdown/js/index.js
+++ src/blocks/dropdown/js/index.js
@@ -1,18 +1,26 @@
 $(document).ready(function(){
     $('.dropdown').on('click','.dropdown__placeholder',function(){
         var parent = $(this).closest('.dropdown');
-        if ( ! parent.hasClass('dropdown_open')){
+        if (!parent.hasClass('dropdown_open')){
             parent.addClass('dropdown_open');
             $('.dropdown.dropdown_open').not(parent).removeClass('dropdown_open');
-        }else{
+        }
+        else{
             parent.removeClass('dropdown_open');
         }
         $('.dropdown__placeholder').css("display","none");
+        if($('.dropdown__placeholder').text()=='RU'){
+            $('.dropdown__list').toggleClass('dropdown__list_ru');
+        }
+        else{
+            $('.dropdown__list').removeClass('dropdown__list_ru');
+        }
     }).on('click','ul>li',function(){
         var parent = $(this).closest('.dropdown');
-        parent.removeClass('dropdown_open').find('.dropdown__placeholder').text( $(this).text() );
-        $('input[name=dropdown__input]').attr('value', $(this).attr('data-value') );
+        parent.removeClass('dropdown_open').find('.dropdown__placeholder').text( $(this).text());
+        $('input[name=dropdown__input]').attr('value',$(this).attr('data-value'));
         $('.dropdown__placeholder').css("display","block");
+        $('.dropdown__list').removeClass('dropdown__list_ru');
     });
-    $(".dropdown" ).insertAfter("input[name=dropdown__input]");
+    $(".dropdown").insertAfter("input[name=dropdown__input]");
 });
